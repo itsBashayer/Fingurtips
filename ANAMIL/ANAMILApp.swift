34c0dd737d44b5af2ef7aee4bd99175ff2d268dc
itsBashayer <alfuhaidbashayer@gmail.com>
2025-04-27 09:47:53 +0300

    Initial Commit

---
+++ ANAMIL/ANAMILApp.swift
@@ -0,0 +1,17 @@
+//
+//  ANAMILApp.swift
+//  ANAMIL
+//
+//  Created by BASHAER AZIZ on 29/10/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ANAMILApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ANAMIL/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ANAMIL
+//
+//  Created by BASHAER AZIZ on 29/10/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
